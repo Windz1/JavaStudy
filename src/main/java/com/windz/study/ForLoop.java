c539c66dd8ee81dbd137aa5f2b479eddaa89aec4
Windz1 <z13777939343@163.com>
2024-07-16 21:29:58 +0800

    远程测试更新

--- src/main/java/com/windz/study/ForLoop.java
+++ src/main/java/com/windz/study/ForLoop.java
@@ -4,6 +4,7 @@ public class ForLoop {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
             System.out.println(i);
+            // 测试远程更新，这句话是在远程写入的
         }
     }
 }
