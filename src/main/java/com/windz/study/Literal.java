c71d7cf0331d98458808c710cdb57ca78dc518b1
zry <z13777939343@163.com>
2024-07-17 19:08:00 +0800

    2024/07/17字面量

---
+++ src/main/java/com/windz/study/Literal.java
@@ -0,0 +1,11 @@
+package com.windz.study;
+
+public class Literal {
+    public static void main(String[] args) {
+        System.out.println(10);//整型
+        System.out.println(1.1);//小数
+        System.out.println('a');//字符
+        System.out.println(true);//布尔值
+        System.out.println("字符串");//字符串
+    }
+}
