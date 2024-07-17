8e75a826799c58a276c9d034176bd8908de47fad
zry <z13777939343@163.com>
2024-07-17 19:08:30 +0800

    2024/07/17 变量定义

---
+++ src/main/java/com/windz/study/Scr.java
@@ -0,0 +1,17 @@
+package com.windz.study;
+
+import java.util.Scanner;
+
+public class Scr {
+    public static void main(String[] args) {
+
+        Scanner math = new Scanner(System.in);
+
+        System.out.println("请输入一个数字:");
+        int i = math.nextInt();
+
+        System.out.println(i);
+
+    }
+
+}
---
+++ src/main/java/com/windz/study/variable.java
@@ -0,0 +1,36 @@
+package com.windz.study;
+
+public class variable {
+     public static void main(String[] args) {
+
+
+         //2.定义short类型的变量
+         short b = 20;
+         System.out.println(b);
+
+         //3.定义int类型的变量
+         int c = 30;
+         System.out.println(c);
+
+         //4.定义long类型的变量,最后需要加大写L
+         long d = 123456789123456789L;
+         System.out.println(d);
+
+         //5.定义float类型的变量，最后需要加大写F
+         float e = 10.1F;
+         System.out.println(e);
+
+         //6.定义double类型的变量
+         double f = 20.3;
+         System.out.println(f);
+
+         //7.定义char类型的变量
+         char g = 'a';
+         System.out.println(g);
+
+         //8.定义boolean类型的变量
+         boolean h = true;
+         System.out.println(h);
+
+    }
+}
