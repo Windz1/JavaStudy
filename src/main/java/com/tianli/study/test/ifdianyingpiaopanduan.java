a5e1059df2b6fd925511abf2ee4efd544df5ab83
tianli2005 <2074272399@qq.com>
2024-09-22 22:51:42 +0800

    电影票座位判断

---
+++ src/main/java/com/tianli/study/test/ifdianyingpiaopanduan.java
@@ -0,0 +1,22 @@
+package com.tianli.study.test;
+
+import java.util.Scanner;
+
+public class ifdianyingpiaopanduan {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请录入一个票号");
+        int ticker = sc.nextInt();
+
+        if (ticker >= 0 && ticker <= 100 ){
+
+          if (ticker % 2 == 0 ){
+            System.out.println("坐右边");}
+          else{
+            System.out.println("坐左边");
+          }
+        }
+
+
+}
+}
